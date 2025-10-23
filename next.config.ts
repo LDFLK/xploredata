d079d0d963a5ba5bdd99bcf58d603ab886eceb3e
Chamith Chanuka <chamithroxmpc@gmail.com>
2025-10-23 09:30:39 +0530

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // reactStrictMode: true,
   output: "export",
-  distDir: 'dist',
   /* config options here */
   eslint: {
     // Ignore ESLint errors during production builds
